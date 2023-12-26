0dede9f71ec9bfc8246996dd6f97756c0edb888f
SANUSI ABUBAKR <sanusiabubakr343@gmail.com>
2023-12-26 10:42:59 +0100

    fixed resources

--- resources/models.py
+++ resources/models.py
@@ -9,7 +9,7 @@ class ResourceType(models.Model):
     name = models.CharField(max_length=50, null=True)
 
     def __str__(self) -> str:
-        return self.type
+        return self.name
 
 
 class Resources(models.Model):
