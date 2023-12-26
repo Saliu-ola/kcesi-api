8f76c90237f131d41fe30ec9c91d9e381fb94ab8
SANUSI ABUBAKR <sanusiabubakr343@gmail.com>
2023-12-26 09:42:56 +0100

    modified admin dashboard for in app chat

--- in_app_chat/models.py
+++ in_app_chat/models.py
@@ -7,7 +7,7 @@ from organization.models import Organization
 class InAppChat(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sender_chats")
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="receiver_chats")
-    message = models.TextField(null=True, verbose_name="Chat contents")  
+    message = models.TextField(null=True, verbose_name="Chat contents")
     organization = models.ForeignKey(
         Organization, on_delete=models.CASCADE, related_name="organization_chats"
     )
@@ -17,7 +17,7 @@ class InAppChat(models.Model):
     updated_at = models.DateTimeField(auto_now=True, null=True)
 
     def __str__(self) -> str:
-        return f"{self.sender__email} --> {self.receiver__email}"
+        return f"{self.sender} --> {self.receiver}"
 
     class Meta:
         ordering = ["-created_at"]
