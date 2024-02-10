42fc9318632c47f835a8eda7a52a1313bb1a61ed
SANUSI ABUBAKR <sanusiabubakr343@gmail.com>
2024-02-10 08:28:39 +0100

    added comments to forums

--- forum/views.py
+++ forum/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import generics, mixins, status
 from rest_framework.decorators import APIView, api_view, permission_classes
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny,IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
-from .models import Forum
-from .serializers import ForumSerializer
+from .models import Forum, ForumComment
+from .serializers import ForumSerializer, ForumCommentSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics, status, viewsets, filters
 from rest_framework.decorators import action
@@ -17,7 +17,7 @@ from drf_spectacular.types import OpenApiTypes
 class ForumViewSets(viewsets.ModelViewSet):
     http_method_names = ["get", "patch", "post", "put", "delete"]
     serializer_class = ForumSerializer
-    permission_classes = [IsSuperAdminOrAdmin]
+    permission_classes = [IsAuthenticated]
     queryset = Forum.objects.all()
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_fields = ['category', 'user', 'organization', 'group']
@@ -117,3 +117,22 @@ class ForumViewSets(viewsets.ModelViewSet):
             {"success": True, "total_forums": output},
             status=status.HTTP_200_OK,
         )
+
+
+class CommentViewSets(viewsets.ModelViewSet):
+    http_method_names = ["get", "patch", "post", "put", "delete"]
+    serializer_class = ForumCommentSerializer
+    permission_classes = [IsAuthenticated]
+    queryset = ForumComment.objects.all()
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['user', 'organization', 'forum', 'group']
+    search_fields = ['content']
+    ordering_fields = ['created_at']
+
+    def paginate_results(self, queryset):
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
