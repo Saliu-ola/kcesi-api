bdb32ee214888b853790b1f8c83e26fae1e25988
SANUSI ABUBAKR <sanusiabubakr343@gmail.com>
2024-03-30 11:36:31 +0100

    added group_name and organization name

--- in_app_chat/serializers.py
+++ in_app_chat/serializers.py
@@ -6,6 +6,12 @@ from .models import InAppChat
 class InAppChatSerializer(serializers.ModelSerializer):
     sender_full_name = serializers.StringRelatedField(source='sender.full_name')
     receiver_full_name = serializers.StringRelatedField(source='receiver.full_name')
+    group_name = serializers.StringRelatedField(
+        source='group.title',
+    )
+    organization_name = serializers.StringRelatedField(
+        source='organization.name',
+    )
 
     class Meta:
         model = InAppChat
