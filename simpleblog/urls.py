cb1c67976b6109ae38615f650f3de178b30065da
SANUSI ABUBAKR <sanusiabubakr343@gmail.com>
2024-04-24 10:35:05 +0100

    initiate v2 deployment config

--- simpleblog/urls.py
+++ simpleblog/urls.py
@@ -18,6 +18,6 @@ urlpatterns = [
     path("feedbacks/", include("feedback.urls")),
     path("auth/", include("accounts.urls")),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/v1/doc/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/v1/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('api/v2/doc/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/v2/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
