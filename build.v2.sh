af0c77ed923ce736829e456728cde9ebeef8ef4e
SANUSI ABUBAKR <sanusiabubakr343@gmail.com>
2024-04-25 22:40:37 +0100

    daphne

--- build.v2.sh
+++ build.v2.sh
@@ -5,8 +5,6 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
-pip install -U channels_redis
-
 # Convert static asset files
 python manage.py collectstatic --no-input
 
