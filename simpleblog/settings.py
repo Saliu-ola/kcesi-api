b3ed135b782032817835616edfa96c2e3b35f271
SANUSI ABUBAKR <sanusiabubakr343@gmail.com>
2024-04-25 11:55:43 +0100

    added channels layer

--- simpleblog/settings.py
+++ simpleblog/settings.py
@@ -25,6 +25,7 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'daphne',
+    'channels',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -32,7 +33,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "group",
-    'channels',
     "chat",
     "accounts",
     "organization",
