a67cf79de31375513bfc766342b415e7ecb49784
SANUSI ABUBAKR <sanusiabubakr343@gmail.com>
2024-04-25 13:09:05 +0100

    downgraded to use python 3.7

--- build.v2.sh
+++ build.v2.sh
@@ -12,16 +12,16 @@ pip install --upgrade pip
 pip install --upgrade -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --no-input
+/opt/render/project/src/.venv/bin/python3.7 manage.py collectstatic --no-input
 
 # Make database migrations
-python manage.py makemigrations
+/opt/render/project/src/.venv/bin/python3.7 manage.py makemigrations
 
 # Apply database migrations
-python manage.py migrate
+/opt/render/project/src/.venv/bin/python3.7 manage.py migrate
 
 # Dump data (excluding contenttypes) to fixture.json
-# python manage.py dumpdata --exclude=contenttypes > fixture.json
+# /opt/render/project/src/.venv/bin/python3.7 manage.py dumpdata --exclude=contenttypes > fixture.json
 
 # Debugging: Echo environment variables
 # echo "DJANGO_SUPERUSER_USERNAME: $DJANGO_SUPERUSER_USERNAME"
@@ -30,7 +30,7 @@ python manage.py migrate
 
 # # Create superuser if environment variables are provided
 # if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_EMAIL" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
-#     DJANGO_SUPERUSER_PASSWORD="$DJANGO_SUPERUSER_PASSWORD" python manage.py createsuperuser --noinput --username "$DJANGO_SUPERUSER_USERNAME" --email "$DJANGO_SUPERUSER_EMAIL"
+#     DJANGO_SUPERUSER_PASSWORD="$DJANGO_SUPERUSER_PASSWORD" /opt/render/project/src/.venv/bin/python3.7 manage.py createsuperuser --noinput --username "$DJANGO_SUPERUSER_USERNAME" --email "$DJANGO_SUPERUSER_EMAIL"
 # fi
 
 # Deactivate virtual environment
