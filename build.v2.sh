a4c8b0aff0988ed3beb90905fac0d8508baa5d69
Tiamiyu Abdulmujeeb Olamilekan <abdulmujeebtiamiyu@gmail.com>
2024-06-18 07:39:22 +0100

    twisted support

--- build.v2.sh
+++ build.v2.sh
@@ -1,8 +1,28 @@
+# #!/usr/bin/env bash
+# # Exit on error
+# set -o errexit
+
+# # Modify this line as needed for your package manager (pip, poetry, etc.)
+# pip install -r requirements.txt
+# pip install -U 'channels[daphne]' channels-redis
+
+# # Convert static asset files
+# python manage.py collectstatic --no-input
+
+# # Apply any outstanding database migrations
+# python manage.py migrate
+
+
 #!/usr/bin/env bash
 # Exit on error
 set -o errexit
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
+# Install system dependencies
+apt-get update
+apt-get install -y build-essential python3-dev libffi-dev libssl-dev
+
+# Install Python dependencies
+pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
 pip install -U 'channels[daphne]' channels-redis
 
