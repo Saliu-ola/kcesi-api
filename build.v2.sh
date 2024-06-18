4dbb6014e321c3d0f8821dd22096bf2ecd3e2a2a
Tiamiyu Abdulmujeeb Olamilekan <abdulmujeebtiamiyu@gmail.com>
2024-06-18 08:02:10 +0100

    fixing

--- build.v2.sh
+++ build.v2.sh
@@ -1,28 +1,8 @@
-# #!/usr/bin/env bash
-# # Exit on error
-# set -o errexit
-
-# # Modify this line as needed for your package manager (pip, poetry, etc.)
-# pip install -r requirements.txt
-# pip install -U 'channels[daphne]' channels-redis
-
-# # Convert static asset files
-# python manage.py collectstatic --no-input
-
-# # Apply any outstanding database migrations
-# python manage.py migrate
-
-
 #!/usr/bin/env bash
 # Exit on error
 set -o errexit
 
-# Install system dependencies
-apt-get update
-apt-get install -y build-essential python3-dev libffi-dev libssl-dev
-
-# Install Python dependencies
-pip install --upgrade pip setuptools wheel
+# Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 pip install -U 'channels[daphne]' channels-redis
 
@@ -31,3 +11,4 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+t
