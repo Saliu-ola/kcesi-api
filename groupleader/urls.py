4e1853c4ea7deaa4f8ddcc93ea14c4a00b644548
Tiamiyu Abdulmujeeb Olamilekan <abdulmujeebtiamiyu@gmail.com>
2024-06-23 22:40:48 +0100

    return user object for groupleader endpoint

--- groupleader/urls.py
+++ groupleader/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     path('group-leader/', GroupLeaderListCreateView.as_view(), name='group-leader-list-create'),
     path('group-leader/<int:group_id>/', GroupLeaderRetrieveUpdateDestroyView.as_view(), name='group-leader-detail'),
     path('library-options/', LibraryOptionListCreateView.as_view(), name='library-option-list-create'),
-    path('library-options/<int:pk>/', LibraryOptionRetrieveUpdateDestroyView.as_view(), name='library-option-detail'),
+    path('library-options/<int:group_id>/', LibraryOptionRetrieveUpdateDestroyView.as_view(), name='library-option-detail'),
     path('library-files/', LibraryFileListCreateView.as_view(), name='library-file-list-create'),
-    path('library-files/<int:pk>/', LibraryFileRetrieveUpdateDestroyView.as_view(), name='library-file-detail'),
+    path('library-files/<int:group_id>/', LibraryFileRetrieveUpdateDestroyView.as_view(), name='library-file-detail'),
 
     path('process-library-files/', ProcessLibraryFiles.as_view(), name='process-library-files'),
 ]
