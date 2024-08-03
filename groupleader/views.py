ae926a9aaec16cf2b060092fcfcb7cbd850fb750
Tiamiyu Abdulmujeeb Olamilekan <abdulmujeebtiamiyu@gmail.com>
2024-08-03 11:08:04 +0100

    error handling

--- groupleader/views.py
+++ groupleader/views.py
@@ -75,22 +75,37 @@ class LibraryFileListCreateView(generics.ListCreateAPIView):
             return LibraryFile.objects.filter(group=group)
         else:
             return LibraryFile.objects.filter(group=group, user=self.request.user)
-        
+
+    # def list(self, request, *args, **kwargs):
+    #     queryset = self.get_queryset()
+    #     page = self.paginate_queryset(queryset)
+    #     if page is not None:
+    #         serializer = self.get_serializer(page, many=True)
+    #         return self.get_paginated_response(self.add_is_group_leader(serializer.data))
+    #     serializer = self.get_serializer(queryset, many=True)
+    #     return Response(self.add_is_group_leader(serializer.data))
     def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
+        try:
+            queryset = self.get_queryset()
+        except ValidationError as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
         page = self.paginate_queryset(queryset)
         if page is not None:
             serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(self.add_is_group_leader(serializer.data))
+            return self.get_paginated_response(
+                self.add_is_group_leader(serializer.data)
+            )
+
         serializer = self.get_serializer(queryset, many=True)
         return Response(self.add_is_group_leader(serializer.data))
-    
+
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         return Response(self.add_is_group_leader([serializer.data])[0], status=status.HTTP_201_CREATED)
-    
+
     def perform_create(self, serializer):
         group_id = self.kwargs.get('group_id')
         group = get_object_or_404(Group, id=group_id)
